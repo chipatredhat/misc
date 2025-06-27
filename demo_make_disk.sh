2e1359cfdd6bc2ac9ec938cfcba450942a9fa670
chipatredhat <chip@redhat.com>
2025-06-27 13:56:27 -0700

    Create demo_make_disk.sh

---
+++ demo_make_disk.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# This script should only be used to make a disk in the red hat demo environment when deploying the Image Mode Workshop
+
+DISK=$(lsblk | grep G | grep -Ev 'vda|loop' | awk '{print $1}')
+echo 'type=83' | sudo sfdisk /dev/${DISK}
+sudo mkfs.xfs /dev/${DISK}1
+echo "/dev/${DISK}1 /var/lib/libvirt/images xfs defaults 0 0" | sudo tee -a /etc/fstab
+sudo systemctl daemon-reload
+sudo mount -a
