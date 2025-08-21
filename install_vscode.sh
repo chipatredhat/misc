1b924a42133c6b2c4792cbfeaae5375a0afa7757
chipatredhat <chip@redhat.com>
2025-08-21 12:50:36 -0700

    Update install_vscode.sh

--- install_vscode.sh
+++ install_vscode.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# curl -s https://raw.githubusercontent.com/chipatredhat/misc/refs/heads/main/install_vscode.sh | bash
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 sudo dnf -y install code
